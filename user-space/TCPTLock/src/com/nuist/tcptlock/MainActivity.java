14d537327d0e003572d8f2b83b6a6e9ef908f8da
AlexanderWangGit <wg1033755123@gmail.com>
2014-11-15 19:07:41 +0800

    add lockScreen

--- user-space/TCPTLock/src/com/nuist/tcptlock/MainActivity.java
+++ user-space/TCPTLock/src/com/nuist/tcptlock/MainActivity.java
@@ -1,11 +1,16 @@
 package com.nuist.tcptlock;
 
+import com.nuist.tcptlock.LockScreenService;
+import com.nuist.tcptlock.MainActivity;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.KeyEvent;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -15,20 +20,28 @@ import android.widget.ImageView;
 
 public class MainActivity extends Activity implements OnClickListener {
 
-	private MyInstalledReceiver installedReceiver = null;
 	private EditText password;
 	private ImageButton setting;
+	private Intent intentService;
+	public static final int LOCKED_SUCCESS=1;
+	public static final int VIEW_INVALIDATE=2;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		// 取消标题栏
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		// 全屏
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+		// 全屏显示
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+		
 		setContentView(R.layout.lock);
 		initView();
+		startService();
+
 	}
 
 	private void initView(){
@@ -40,7 +53,65 @@ public class MainActivity extends Activity implements OnClickListener {
 		setting.setOnClickListener(this);
 	}
 	
+	
+    /*验证密码并结束activity回到主屏*/
+	/*
+	 * 
+	 
+    public void validate()
+    {
+    	if(){//验证成功
+    		MainActivity.this.finish();
+    	}else if(){
+    		//验证失败
+    	}
+    }
+    
+    */
+
+
+	/**
+	 * 启动服务
+	 */
+	private void startService() {
+		// TODO Auto-generated method stub
+		intentService = new Intent(MainActivity.this, LockScreenService.class);
+		startService(intentService);
+	}
+
+	/**
+	 * 
+	 * 屏蔽掉Home键
+	 * 
+	 */
 	@Override
+	public void onAttachedToWindow() {
+		// TODO Auto-generated method stub
+//		getWindow().setType(WindowManager.LayoutParams.TYPE_KEYGUARD_DIALOG);
+		super.onAttachedToWindow();
+	}
+	
+	/**
+	 * 
+	 * 屏蔽掉返回键
+	 * 
+	 */
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// TODO Auto-generated method stub
+		
+		if(keyCode==KeyEvent.KEYCODE_BACK){
+			return true;
+		}else {
+			return super.onKeyDown(keyCode, event);	
+		}
+		
+	}
+	
+	
+	/*
+	 *@Override
+
 	public void onStart() {
 		super.onStart();
 
@@ -53,15 +124,9 @@ public class MainActivity extends Activity implements OnClickListener {
 
 		this.registerReceiver(installedReceiver, filter);
 	}
+	*/
 
-	@Override
-	public void onDestroy() {
-		if (installedReceiver != null) {
-			this.unregisterReceiver(installedReceiver);
-		}
-
-		super.onDestroy();
-	}
+	
 
 	@Override
 	public void onClick(View v) {
